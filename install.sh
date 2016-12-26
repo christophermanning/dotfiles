ab9644d0df3e7251fac8b4113fdf717fd56c3b87
Christopher Manning <c@christophermanning.org>
2016-12-26 14:57:31 -0600

    install.sh: update desktop preferences

--- install.sh
+++ install.sh
@@ -39,7 +39,43 @@ else
   if [ "$GUI" == 1 ]; then
     # install gui packages
     sudo apt-get -y install vim-gnome chromium-browser
-    sudo ln -sf /etc/alternatives/x-www-browser /usr/bin/google-chrome
+    # install fonts
+    mkdir ~/.fonts
+    pushd ~/.fonts
+    wget -N https://github.com/powerline/fonts/raw/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf
+    fc-cache -vf ~/.fonts
+    popd
+
+    # terminal preferences
+    uid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d '\''') && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ foreground-color 'rgb(131,148,150)' && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ use-system-font false && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ use-theme-colors false && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ use-theme-transparency false && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ font 'Meslo LG M DZ for Powerline 12' && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ scrollback-unlimited true && \
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uid/ background-color 'rgb(0,43,54)'
+
+    # desktop preferences
+    gsettings set org.gnome.desktop.search-providers disabled "['org.gnome.Software.desktop']"
+    gsettings set org.gnome.shell favorite-apps "['chromium-browser.desktop', 'org.gnome.Nautilus.desktop', 'gnome-terminal.desktop']"
+
+    # disable bluetooth
+    rfkill block bluetooth
+
+    # disable touchpad
+    gsettings set org.gnome.desktop.peripherals.touchpad send-events 'disabled'
+    sudo modprobe -r psmouse && sudo modprobe psmouse
+
+    # caps to escape
+    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+
+    # 12 hour clock
+    gsettings set org.gnome.desktop.interface clock-format 12h
+
+    # TODO: disable alert sound
+    # pactl play-sample audio-volume-change
+    gsettings set org.gnome.desktop.sound event-sounds false
   fi
 fi
 
