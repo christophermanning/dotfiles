5fd020b60a64fff817ab4340572d4129c80b7283
Christopher Manning <c@christophermanning.org>
2016-12-26 14:58:55 -0600

    install.sh: update and add packages

--- install.sh
+++ install.sh
@@ -26,19 +26,48 @@ if [[ "$OSTYPE" == darwin* ]]; then
 else
   # install packages
   sudo apt-get -y update
+
+  # install updates since release
+  sudo unattended-upgrades -v
+
+  # install preferred packages
   sudo apt-get -y install git curl tree htop ctags tmux tig silversearcher-ag ethtool xclip zsh mtr
 
   # install rcm
   pushd /tmp
-  wget -N https://thoughtbot.github.io/rcm/debs/rcm_1.2.3-1_all.deb
-  sha=$(sha256sum rcm_1.2.3-1_all.deb | cut -f1 -d' ')
-  [ "$sha" = "fb8ec2611cd4d519965b66fcf950bd93d7593773659f83a8612053217daa38b4" ] && \
-  sudo dpkg -i rcm_1.2.3-1_all.deb
+  wget -N https://thoughtbot.github.io/rcm/debs/rcm_1.3.0-1_all.deb
+  sha=$(sha256sum rcm_1.3.0-1_all.deb | cut -f1 -d' ')
+  [ "$sha" = "2e95bbc23da4a0b995ec4757e0920197f4c92357214a65fedaf24274cda6806d" ] && \
+  sudo dpkg -i rcm_1.3.0-1_all.deb
   popd
 
   if [ "$GUI" == 1 ]; then
     # install gui packages
     sudo apt-get -y install vim-gnome chromium-browser
+
+    # install virtualbox
+    sudo tee /etc/apt/sources.list.d/virtualbox.list <<EOF
+deb http://download.virtualbox.org/virtualbox/debian xenial contrib
+EOF
+
+    pushd /tmp && \
+    wget -N https://www.virtualbox.org/download/oracle_vbox_2016.asc && \
+    fingerprint=$(gpg --with-fingerprint oracle_vbox_2016.asc | grep Key |  awk -F'= ' '{print $2}') && \
+    [ "$fingerprint" = "B9F8 D658 297A F3EF C18D  5CDF A2F6 83C5 2980 AECF" ] && \
+    sudo apt-key add oracle_vbox_2016.asc && \
+    popd
+
+    sudo apt-get update
+    sudo apt-get -y install virtualbox-5.1
+
+    # install vagrant
+    pushd /tmp
+    wget -N https://releases.hashicorp.com/vagrant/1.9.1/vagrant_1.9.1_x86_64.deb
+    sha=$(sha256sum vagrant_1.9.1_x86_64.deb | cut -f1 -d' ')
+    [ "$sha" = "d006d6227e049725b64d8ba3967f0c82460a403ff40230515c93134d58723150" ] && \
+    sudo dpkg -i vagrant_1.9.1_x86_64.deb
+    popd
+
     # install fonts
     mkdir ~/.fonts
     pushd ~/.fonts
@@ -115,4 +144,8 @@ if [ -t 1 ]; then
   echo "export GIT_AUTHOR_EMAIL=\"$git_email\" && export GIT_COMMITTER_EMAIL=\$GIT_AUTHOR_EMAIL" >> ~/.zshrc.local
 fi
 
+# install private packages
 [[ -f install_private.sh ]] && ./install_private.sh
+
+# cleanup
+sudo apt-get -y autoremove
