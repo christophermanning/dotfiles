a6eb6aa3d8bd8cd79b76a078df41bc0c40577604
Christopher Manning <c@christophermanning.org>
2017-01-06 12:57:43 -0600

    install.sh: docker

--- install.sh
+++ install.sh
@@ -68,6 +68,18 @@ EOF
     sudo dpkg -i vagrant_1.9.1_x86_64.deb
     popd
 
+    # install docker
+    sudo apt-get -y install apt-transport-https ca-certificates linux-image-extra-$(uname -r) linux-image-extra-virtual
+    sudo apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+    sudo apt-get update
+    sudo apt-get -y install docker-engine
+    sudo service docker start
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+
     # install fonts
     mkdir ~/.fonts
     pushd ~/.fonts
